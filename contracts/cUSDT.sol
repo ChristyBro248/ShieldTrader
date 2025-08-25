5ee047835e8f3b846131dc98f1b4a922dc27dc37
ChristyBro248 <192625168+ChristyBro248@users.noreply.github.com>
2025-08-25 18:29:59 +0800

    use cUSDT as faucet itself

--- contracts/cUSDT.sol
+++ contracts/cUSDT.sol
@@ -8,8 +8,15 @@ import {ConfidentialFungibleToken} from "@openzeppelin/confidential-contracts/to
 import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+import {FHE, externalEuint64, euint64} from "@fhevm/solidity/lib/FHE.sol";
+
 contract cUSDT is ConfidentialFungibleTokenERC20Wrapper, SepoliaConfig {
     constructor(
         IERC20 usdt
     ) ConfidentialFungibleTokenERC20Wrapper(usdt) ConfidentialFungibleToken("cUSDT", "cUSDT", "") {}
+
+    // _mint(address to, euint64 amount)
+    function faucet() external {
+        _mint(msg.sender, FHE.asEuint64(1000 * 10 ** 6));
+    }
 }
