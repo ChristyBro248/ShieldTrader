43281f776fc3fb69ffdf8b5d36268cc6db676586
TiffanyEvan <191904169+TiffanyEvan@users.noreply.github.com>
2025-08-25 18:08:00 +0800

    deploy to sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,17 +6,17 @@ import "@typechain/hardhat";
 import "hardhat-deploy";
 import "hardhat-gas-reporter";
 import type { HardhatUserConfig } from "hardhat/config";
-import { vars } from "hardhat/config";
 import "solidity-coverage";
+import { config as dotenvConfig } from "dotenv";
+
+dotenvConfig();
 
 import "./tasks/accounts";
 import "./tasks/FHECounter";
 import "./tasks/LeadTrading";
 
-// Run 'npx hardhat vars setup' to see the list of variables that need to be set
-
-const MNEMONIC: string = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
-const INFURA_API_KEY: string = vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+// Load environment variables from .env fil
+const Alchemy_API_KEY: string = process.env.Alchemy_API_KEY || "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -25,7 +25,7 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      sepolia: vars.get("ETHERSCAN_API_KEY", ""),
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
     },
   },
   gasReporter: {
@@ -35,28 +35,12 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhat: {
-      accounts: {
-        mnemonic: MNEMONIC,
-      },
       chainId: 31337,
-    },
-    anvil: {
-      accounts: {
-        mnemonic: MNEMONIC,
-        path: "m/44'/60'/0'/0/",
-        count: 10,
-      },
-      chainId: 31337,
-      url: "http://localhost:8545",
     },
     sepolia: {
-      accounts: {
-        mnemonic: MNEMONIC,
-        path: "m/44'/60'/0'/0/",
-        count: 10,
-      },
+      accounts: [process.env.PRIVATE_KEY!],
       chainId: 11155111,
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${Alchemy_API_KEY}`,
     },
   },
   paths: {
@@ -66,40 +50,22 @@ const config: HardhatUserConfig = {
     tests: "./test",
   },
   solidity: {
-    compilers: [
-      {
-        version: "0.8.24",
-        settings: {
-          metadata: {
-            // Not including the metadata hash
-            // https://github.com/paulrberg/hardhat-template/issues/31
-            bytecodeHash: "none",
-          },
-          // Disable the optimizer when debugging
-          // https://hardhat.org/hardhat-network/#solidity-optimizer-support
-          optimizer: {
-            enabled: true,
-            runs: 800,
-          },
-          evmVersion: "cancun",
-          viaIR: true,
-        },
+    version: "0.8.27",
+    settings: {
+      metadata: {
+        // Not including the metadata hash
+        // https://github.com/paulrberg/hardhat-template/issues/31
+        bytecodeHash: "none",
       },
-      {
-        version: "0.8.27",
-        settings: {
-          metadata: {
-            bytecodeHash: "none",
-          },
-          optimizer: {
-            enabled: true,
-            runs: 800,
-          },
-          evmVersion: "cancun",
-          viaIR: true,
-        },
+      // Disable the optimizer when debugging
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 800,
       },
-    ],
+      evmVersion: "cancun",
+      viaIR: true,
+    },
   },
   typechain: {
     outDir: "types",
